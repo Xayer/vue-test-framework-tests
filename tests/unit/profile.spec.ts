9851857d9461bf3dd3fc63bc3d9affb4eed62fd6
Frederik Rabøl <frederik-rm@hotmail.com>
2019-08-02 17:05:47 +0200

    moved all files in .jest/ to / instead to simplify project structure

--- tests/unit/profile.spec.ts
+++ tests/unit/profile.spec.ts
@@ -3,14 +3,13 @@ import Vuex, { Store, Module } from 'vuex';
 import Profile from '@/components/profile.vue';
 import { ProfileState } from '@/store/profile/types';
 import { RootState } from '../../src/store/types';
-import { expect } from 'chai';
-import { spy } from 'sinon';
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
 let mockStore: Store<ProfileState>;
 let wrapper: Wrapper<Profile>;
+const fetchData = jest.fn();
 
 describe('Profile.vue', () => {
   beforeEach(() => {
@@ -18,7 +17,7 @@ describe('Profile.vue', () => {
       modules: {
         profile: {
           namespaced: true,
-          actions: { fetchData: spy() },
+          actions: { fetchData },
           getters: {
             firstName : () => 'Test',
             lastName : () => 'User',
@@ -30,22 +29,48 @@ describe('Profile.vue', () => {
               phone: '',
             },
             error: false,
-          },
+          }
         },
       },
     });
+    fetchData.mockClear();
   });
 
   it('Displays Fullname if user is present', () => {
     const fullName = 'Test User';
     createWrapper();
-    expect(wrapper.findAll('p').at(0).find('span').text()).to.have.string(fullName);
+    expect(wrapper.findAll('p').at(0).find('span').text()).toBe(fullName);
   });
 
   it('Displays Fullname if user is present', () => {
     const email = 'test@example.com';
     createWrapper();
-    expect(wrapper.findAll('p').at(1).find('span').text()).to.have.string(email);
+    expect(wrapper.findAll('p').at(1).find('span').text()).toBe(email);
+  });
+
+  it('Displays an error message if an error occured', () => {
+    const errorMessage = 'Oops an error occured';
+    wrapper = shallowMount(Profile, {
+      store: new Vuex.Store({
+        modules: {
+          profile: {
+            namespaced: true,
+            actions: { fetchData },
+            getters: {
+              firstName : () => '',
+              lastName : () => '',
+            },
+            state: {
+              user: {},
+              error: true,
+            }
+          },
+        },
+      }),
+      localVue,
+      sync: false,
+    });
+    expect(wrapper.findAll('div').at(2).text()).toBe(errorMessage);
   });
 
   function createWrapper(options: ShallowMountOptions<Profile> = {}): void {
